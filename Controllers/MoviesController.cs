d8e6a0811efd40df4e7453b6e0ac75736258a47f
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-05-03 17:51:32 +0700

    menambahkan API Movies

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace jurnal10.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private static List<Movie> movieList = new List<Movie>
+        {
+            new Movie("The Shawshank Redemption", "Frank Darabont", new List<string> {
+                        "Tim Robbins", "Morgan Freeman","Bob Gunton" },
+                "A banker convicted of uxoricide forms a friendship over a quarter century with a hardened convict, while maintaining his innocence and trying to remain hopeful through simple compassion.")
+        };
+        // GET: api/<MoviesController>
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            return movieList;
+        }
+
+        // GET api/<MoviesController>/5
+        [HttpGet("{id}")]
+        public Movie Get(int id)
+        {
+            return movieList[id];
+        }
+
+        // POST api/<MoviesController>
+        [HttpPost]
+        public void Post([FromBody] Movie value)
+        {
+            movieList.Add(value);
+        }
+
+        // DELETE api/<MoviesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            movieList.RemoveAt(id);
+        }
+    }
+}
---
+++ Movie.cs
@@ -0,0 +1,16 @@
+﻿namespace jurnal10
+{
+    public class Movie
+    {
+        public string title { get; set; }
+        public string Director { get; set; }
+        public List<string> Star { get; set; }
+        public string Description { get; set; }
+        public Movie(string title, string director, List<string> star, string description) { 
+           this.title = title;
+           this.Director = director;
+           this.Star = star;
+           this.Description = description;
+        }
+    }
+}
